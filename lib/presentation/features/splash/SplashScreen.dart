da65a2053ade3e87a59cc5d1761db5b1c4d92403
shindonghwi <ehdgnl8940@naver.com>
2023-07-08 11:03:59 +0900

    feat: 스플래시 화면 추가

--- lib/presentation/features/splash/SplashScreen.dart
+++ lib/presentation/features/splash/SplashScreen.dart
@@ -3,6 +3,7 @@ import 'package:cassette/navigation/Route.dart';
 import 'package:cassette/presentation/ui/colors.dart';
 import 'package:cassette/presentation/utils/Common.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SplashScreen extends HookConsumerWidget {
@@ -10,20 +11,43 @@ class SplashScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      backgroundColor: getColorScheme(context).black,
-      body: GestureDetector(
-        onTap: (){
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Future.delayed(const Duration(seconds: 2), (){
           Navigator.push(
             context,
-            nextSlideScreen(RoutingScreen.Login.route),
+            nextFadeInOutScreen(RoutingScreen.Login.route),
           );
-        },
-        child: Center(
-          child: Center(
-            child: Text("SplashScreen", style: getTextTheme(context).labelSmall,),
+        });
+      });
+      return null;
+    }, []);
+
+    return Scaffold(
+      backgroundColor: getColorScheme(context).bg,
+      body: Stack(
+        children: [
+          Container(
+            width: getMediaQuery(context).size.width,
+            height: getMediaQuery(context).size.height * 0.32,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                const Color(0xFF7C96FC).withOpacity(0.8),
+                const Color(0xFF4C6DEC).withOpacity(0.0),
+              ],
+            )),
           ),
-        ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, getMediaQuery(context).size.height * 0.3, 0, 0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset("assets/imgs/image_splash.png"),
+            ),
+          )
+        ],
       ),
     );
   }
